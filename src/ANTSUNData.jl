eb7652441f0e88f89739ab144ab5a94668e250a8
Jungsoo Kim <jungsu93@gmail.com>
2022-12-05 13:32:51 -0500

    modify Project.toml

--- src/ANTSUNData.jl
+++ src/ANTSUNData.jl
@@ -1,8 +1,13 @@
 module ANTSUNData
-using FlavellConstants, FlavellBase, HDF5
+using FlavellConstants, FlavellBase, HDF5, JLD2, BehaviorDataNIR,
+    StatsBase, ProgressMeter, CaAnalysis, Statistics, Impute, PyPlot
+
 
 include("data_h5.jl")
 # data_h5.jl
-export import_data
+export import_data,
+    export_nd2_h5,
 
+    # data_jld2.jl
+    import_jld2_data
 end # module
--- src/data_h5.jl
+++ src/data_h5.jl
@@ -77,3 +77,105 @@ function import_data(path_h5; import_pumping::Bool=true, std_method=:global,
     
     dict_
 end
+
+function export_nd2_h5(path_data_dict::String; path_h5::Union{String,Nothing}=nothing,
+        path_data_dict_match::Union{String,Nothing}=nothing, n_recording::Int=1,
+        jld2_dict_key::String="dict_key", velocity_filter_threshold=0.2)
+    data_dict = import_jld2_data(path_data_dict, jld2_dict_k);
+
+    idx_splits = [1:data_dict["max_t"]]
+
+    # traces
+    trace_F20 = trace_array_rm_nan_neuron(data_dict["traces_array_F_F20"])[1]
+    trace_zstd = trace_array_rm_nan_neuron(data_dict["raw_zscored_traces_array"])[1]
+    trace, match_org_to_skip, match_skip_to_org = trace_array_rm_nan_neuron(data_dict["traces_array"])
+    n_neuron, n_t = size(trace)
+    
+    # velocity
+    velocity = impute_list(data_dict["velocity_stage"])
+
+    reversal_vec = Int.(data_dict["rev_times"] .> 0)
+    reversal_events = let
+        list_start = findall(diff(reversal_vec) .== 1)
+        list_end = findall(diff(reversal_vec) .== -1)
+        if reversal_vec[1] > 0
+            prepend!(list_start, 1)
+        end
+        hcat(list_start, list_end)
+    end
+    
+    # filter velocity
+    velocity_filt = filter_velocity(velocity, velocity_filter_threshold)
+
+    # NeuroPAL
+    neuropal_q = false
+    if isfile(path_data_dict_match)
+        data_dict_match = import_jld2_data(path_data_dict_neuropal, "data_dict")
+        neuropal_q = true
+    end
+        
+    # export hdf5
+    h5open(path_h5, "w") do h5f
+        # traces
+        g1 = create_group(h5f, "gcamp")
+        g1["traces_array_F_F20"] = trace_F20
+        g1["trace_array"] = trace_zstd
+        g1["trace_array_original"] = trace
+
+        g1["idx_splits"] = Array(hcat(map(x->[x[1], x[end]], idx_splits)...)')
+        g1["match_org_to_skip"] = let
+                list_match = []
+                for (k,v) = match_org_to_skip
+                    push!(list_match, [k,v])
+                end
+
+                Array(hcat(list_match...)')
+            end
+        g1["match_skip_to_org"] = let
+            list_match = []
+            for (k,v) = match_skip_to_org
+                push!(list_match, [k,v])
+            end
+
+            Array(hcat(list_match...)')
+        end
+
+        # behavior
+        g2 = create_group(h5f, "behavior")
+        g2["velocity"] = velocity_filt
+        g2["reversal_vec"] = reversal_vec
+        g2["reversal_events"] = reversal_events
+
+        list_key = ["head_angle", "angular_velocity", "pumping", "worm_curvature",
+            "body_angle_absolute", "body_angle_all", "body_angle"]
+
+        for k = list_key
+            try
+                d = data_dict[k]
+                g2[k] = isa(d, Array{Any}) ? concrete_type(d) : d
+            catch e
+                @warn "Error saving $(k): $e"
+            end
+        end
+
+        # timing
+        g3 = create_group(h5f, "timing")
+        g3["timestamp_nir"] = data_dict["nir_timestamps"]
+        g3["timestamp_confocal"] = data_dict["timestamps"]
+        if haskey(data_dict, "stim_begin_confocal")
+            if length(data_dict["stim_begin_confocal"]) >= 1
+                d = data_dict["stim_begin_confocal"]
+                g3["stim_begin_confocal"] = isa(d, Array{Any}) ? concrete_type(d) : d
+            end
+        end
+
+        # NeuroPAL
+        if neuropal_q
+            g4 = create_group(h5f, "neuropal_registration")
+            g4["roi_match_confidence"] = data_dict_match["roi_match_confidence"]
+            g4["roi_match"] = data_dict_match["roi_matches"]
+        end
+    end
+    
+    nothing
+end
