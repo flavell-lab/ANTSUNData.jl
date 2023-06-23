e7f499d669aae1788f53ead62db0e17c7716ffcb
Jungsoo Kim <jungsu93@gmail.com>
2023-06-23 00:05:55 -0400

    fix typo

--- src/ANTSUNData.jl
+++ src/ANTSUNData.jl
@@ -1,9 +1,10 @@
 module ANTSUNData
-using FlavellConstants, FlavellBase, HDF5,
-    StatsBase, ProgressMeter, Statistics
+    using FlavellConstants, FlavellBase, HDF5,
+        StatsBase, ProgressMeter, Statistics
 
-include("data_h5.jl")
+    include("data_h5.jl")
 
-# data_h5.jl
-export import_data,
+    # data_h5.jl
+    export import_data
+
 end # module
